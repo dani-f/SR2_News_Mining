f9369ed068d1485d53f3b99ee2f9d2a3b22f9802
dani-f <da.gegen@posteo.de>
2021-08-25 18:29:44 +0200

    update scripts

--- R/get_data.R
+++ R/get_data.R
@@ -1,4 +1,3 @@
-### Scraping Data
 # Load pckgs
 library(rvest)
 library(httr)
@@ -12,14 +11,12 @@ URL_Bilanz_am_Abend <- "https://dev2.sr-mediathek.sr-multimedia.de/index.php?sei
 
 # Scrape html
 html_Bilanz_am_Mittag <- read_html(GET(URL_Bilanz_am_Mittag,
-                                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
-                                  user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")))
+                                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L)))
 html_Bilanz_am_Abend <- read_html(GET(URL_Bilanz_am_Abend,
-                                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
-                                  user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")))
+                                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L)))
 
 # Extract data
-# Themen
+## Themen
 Themen_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("div#picturearticle_collection_box p.teaser__text__paragraph") %>% 
   html_text() %>%
@@ -30,7 +27,7 @@ Themen_Abend <- html_Bilanz_am_Abend %>%
   html_text() %>%
   str_remove("^.+(Themen: |Themen:|Themen :|Themen;)|Rep: ")
 
-# Links
+## Links
 Links_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("h3 a") %>%
   html_attr("href")
@@ -40,12 +37,12 @@ Links_Abend <- html_Bilanz_am_Abend %>%
   html_nodes("h3 a") %>% html_attr("href")
 Links_Abend <- paste0("https://dev2.sr-mediathek.sr-multimedia.de/", Links_Abend)
 
-# Länge und Datum
+## Länge und Datum
 Laenge_Datum_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("div#picturearticle_collection_box div.teaser__text__footer__wrapper") %>% 
   html_text() %>% 
   str_extract_all("Länge.{10}|Datum.{12}")
-# Clean Länge und Datum
+### Clean Länge und Datum
 Laenge_Datum_Mittag <-
   matrix(unlist(Laenge_Datum_Mittag),
          nrow = length(Laenge_Datum_Mittag),
@@ -59,7 +56,7 @@ Laenge_Datum_Abend <- html_Bilanz_am_Abend %>%
   html_nodes("div#picturearticle_collection_box div.teaser__text__footer__wrapper") %>% 
   html_text() %>% 
   str_extract_all("Länge.{10}|Datum.{12}")
-# Clean Länge und Datum
+### Clean Länge und Datum
 Laenge_Datum_Abend <-
   matrix(unlist(Laenge_Datum_Abend),
          nrow = length(Laenge_Datum_Abend),
@@ -69,17 +66,16 @@ Laenge_Datum_Abend <-
          Datum = str_trim(str_sub(V2, start = -11))) %>% 
   select(Laenge, Datum)
 
-# Autor
+## Autor
 Autor_Mittag <- 0
 for (i in 1:length(Links_Mittag)) {
   Autor_Mittag[i] <-
     read_html(GET(Links_Mittag[i],
-                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
-                  user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"))) %>%
+                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))) %>%
     html_nodes("div.article__content div p") %>% 
     html_text()
 }
-# Clean Autor
+### Clean Autor
 Autor_Mittag <- Autor_Mittag %>%
   str_extract_all("SR 2 - .+") %>%
   str_sub(start = 8) %>% 
@@ -89,18 +85,17 @@ Autor_Abend <- 0
 for (i in 1:length(Links_Abend)) {
   Autor_Abend[i] <-
     read_html(GET(Links_Abend[i],
-                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
-                  user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"))) %>%
+                  config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))) %>%
     html_nodes("div.article__content div p") %>% 
     html_text()
 }
-# Clean Autor
+### Clean Autor
 Autor_Abend <- Autor_Abend %>%
   str_extract_all("SR 2 - .+") %>%
   str_sub(start = 8) %>% 
   str_trim()
 
-# Create data frame
+# Create data frames
 Bilanz_am_Mittag <- data.frame(Themen = Themen_Mittag,
                                Links = Links_Mittag,
                                Autor = Autor_Mittag,
@@ -117,3 +112,8 @@ Bilanz_am_Abend <- data.frame(Themen = Themen_Abend,
 Bilanz <- bind_rows("Mittag" = Bilanz_am_Mittag,
                     "Abend" = Bilanz_am_Abend,
                     .id = "Format")
+save(Bilanz,
+     file = paste0("data/", "Bilanz_", Sys.Date(), ".Rdata"))
+write.csv2(Bilanz,
+           file = paste0("data/", "Bilanz_", Sys.Date(), ".csv"))
+
