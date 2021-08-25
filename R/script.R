7c09bd29c8c3ae32d59f3251e22c3a5b9824a274
dani-f <da.gegen@posteo.de>
2021-08-25 14:20:56 +0200

    update script

--- R/script.R
+++ R/script.R
@@ -4,42 +4,53 @@ library(rvest)
 library(httr)
 library(dplyr)
 library(stringr)
+library(tibble)
 
 # Pass URLs
 URL_Bilanz_am_Mittag <- "https://dev2.sr-mediathek.sr-multimedia.de/index.php?seite=8&sen=SR2_BAM_P&tbl=pf"
 URL_Bilanz_am_Abend <- "https://dev2.sr-mediathek.sr-multimedia.de/index.php?seite=8&sen=SR2_BAA_P&tbl=pf"
 
 # Scrape html
-Bilanz_am_Mittag <- read_html(GET(URL_Bilanz_am_Mittag,
+html_Bilanz_am_Mittag <- read_html(GET(URL_Bilanz_am_Mittag,
                                   config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
                                   user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")))
-Bilanz_am_Abend <- read_html(GET(URL_Bilanz_am_Abend,
+html_Bilanz_am_Abend <- read_html(GET(URL_Bilanz_am_Abend,
                                   config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
                                   user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")))
 
 # Extract data
 # Themen
-Themen_Mittag <- Bilanz_am_Mittag %>%
+Themen_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("div#picturearticle_collection_box p.teaser__text__paragraph") %>% 
   html_text()
 
 # Links
-Links_Mittag <- Bilanz_am_Mittag %>%
+Links_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("h3 a") %>% html_attr("href")
 
+Links_Mittag <- paste0("https://dev2.sr-mediathek.sr-multimedia.de/", Links_Mittag)
+
 # Länge und Datum
-#Länge_Datum_Mittag <-
-Bilanz_am_Mittag %>%
+Laenge_Datum_Mittag <- html_Bilanz_am_Mittag %>%
   html_nodes("div#picturearticle_collection_box div.teaser__text__footer__wrapper") %>% 
   html_text() %>% 
   str_extract_all("Länge.{10}|Datum.{12}")
+
+Laenge_Datum_Mittag <-
+  matrix(unlist(Laenge_Datum_Mittag),
+         nrow = length(Laenge_Datum_Mittag),
+         byrow = TRUE) %>%
+  as_tibble() %>% 
+  mutate(Laenge = str_trim(str_sub(V1, start = -8)),
+         Datum = str_trim(str_sub(V2, start = -11))) %>% 
+  select(Laenge, Datum)
+
 
 # Autor
 Autor_Mittag <- 0
 for (i in 1:length(Links_Mittag)) {
   Autor_Mittag[i] <-
-    read_html(GET(paste0("https://dev2.sr-mediathek.sr-multimedia.de/",
-                         Links_Mittag[i]),
+    read_html(GET(Links_Mittag[i],
                   config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
                   user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"))) %>%
     html_nodes("div.article__content div p") %>% 
@@ -48,4 +59,12 @@ for (i in 1:length(Links_Mittag)) {
 
 Autor_Mittag <- Autor_Mittag %>%
   str_extract_all("SR 2 - .+") %>%
+  str_sub(start = 8) %>% 
   str_trim()
+
+# Creating data frame
+Bilanz_am_Mittag <- data.frame(Themen = Themen_Mittag,
+                               Links = Links_Mittag,
+                               Autor = Autor_Mittag,
+                               Laenge_Datum_Mittag,
+                               stringsAsFactors = FALSE)
